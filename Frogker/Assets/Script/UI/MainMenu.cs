64aa67ac8de00b4409b9917c777161f26a8fd36b
gbarrantes <gabocano43@gmail.com>
2024-08-16 22:38:38 -0600

    Cambios UI

--- Frogker/Assets/Script/UI/MainMenu.cs
+++ Frogker/Assets/Script/UI/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;//scenes
@@ -7,11 +8,17 @@ using UnityEngine.SceneManagement;//scenes
 public class MainMenu : MonoBehaviour
 {
     #region Variables
-    [Header("Menu setteo")]
+    [Header("Menu principal")]
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _climbMenu;
     [SerializeField] private GameObject _optionsMenu;
     [SerializeField] private GameObject _extrasMenu;
+
+    [Header("Menu opciones")]
+    [SerializeField] private GameObject _audioMenu;
+    [SerializeField] private GameObject _videoMenu;
+    [SerializeField] private GameObject _controlsMenu;
+    [SerializeField] private GameObject _definirMenu;
 
     [Header("Options to scroll")]
     [SerializeField] private GameObject _mainMenuFirst;//Main menu selects climb as it first option
@@ -28,14 +35,13 @@ public class MainMenu : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape) && _optionsMenu == true) Menu();
         if (Input.GetKeyDown(KeyCode.Escape) && _extrasMenu == true) Menu();
 
+        //if (EventSystem.current.currentSelectedGameObject == _audioMenu) _audioMenu.SetActive(true);
+        if (EventSystem.current.currentSelectedGameObject == _videoMenu) _videoMenu.SetActive(true);
+        if (EventSystem.current.currentSelectedGameObject == _controlsMenu) _controlsMenu.SetActive(true);
     }
     private void Start()
     {
-        _mainMenu.SetActive(true);
-        _climbMenu.SetActive(false);
-        _optionsMenu.SetActive(false);
-        _extrasMenu.SetActive(false);
-
+        Menu();
         EventSystem.current.SetSelectedGameObject(_mainMenuFirst);
     }
 
@@ -45,11 +51,19 @@ public class MainMenu : MonoBehaviour
 
     private void Menu() 
     { 
+        //menu principal
         _climbMenu.SetActive(false);
         _optionsMenu.SetActive(false);
         _extrasMenu.SetActive(false);
-        _mainMenu.SetActive(true);
 
+        //menu de opciones
+        _audioMenu.SetActive(false);
+        _videoMenu.SetActive(false);
+        _controlsMenu.SetActive(false);
+        _definirMenu.SetActive(false);
+
+
+        _mainMenu.SetActive(true);
         EventSystem.current.SetSelectedGameObject(_mainMenuFirst);
     }
 
@@ -65,6 +79,7 @@ public class MainMenu : MonoBehaviour
     {
         _mainMenu.SetActive(false);
         _optionsMenu.SetActive(true);
+        _audioMenu.SetActive(true);
 
         EventSystem.current.SetSelectedGameObject(_optionsMenuFirst);
 
@@ -80,7 +95,16 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
-    
+
+
+    #endregion
+
+
+    #region Settings Interaction
+
+
+
+
 
     #endregion
 }
